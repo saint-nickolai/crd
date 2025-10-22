30f54a2b24d53c9d20e51c7b63274de4c7c2eab7
saint-nickolai <139720223+saint-nickolai@users.noreply.github.com>
2025-10-21 19:21:48 -0700

    added ability to compile with arguments

--- crd.cpp
+++ crd.cpp
@@ -1,7 +1,7 @@
 /*
-  cnr.cpp
-  C++ compile and run
-  Nick Howard, Oct 17, 2025
+  crd.cpp
+  C++ Compile, Run, and Delete 
+  Nick Howard, Oct 21, 2025
 */
 
 #include <iostream>
@@ -12,7 +12,6 @@
 using namespace std;
 
 int main(int argc, char* argv[]){
-
   string command = "c++ ";
   string fileArg = argv[1]; 
   string compile = command + fileArg; 
@@ -22,8 +21,16 @@ int main(int argc, char* argv[]){
   chrono::milliseconds timespan(1000);
   this_thread::sleep_for(timespan);
 
-  pipe = popen("./a.out", "r");
-  
+  if (argc > 2) {
+    string a = "./a.out", b;
+    for (int i = 2; i < argc; i++) {
+      b = argv[i];
+      a = a + " " + b;
+    }
+    pipe = popen(a.c_str(), "r");
+  } else 
+    pipe = popen("./a.out", "r");
+
   char buffer[128];
   while (fgets(buffer, sizeof(buffer), pipe) != NULL)
     printf("%s", buffer);
